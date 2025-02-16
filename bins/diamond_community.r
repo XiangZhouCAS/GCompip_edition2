dcd1f102ac5f7b4b868a3d8428b2c9ce051a6541
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2025-02-16 18:38:31 +0800

    Update diamond_community.r

--- bins/diamond_community.r
+++ bins/diamond_community.r
@@ -215,7 +215,7 @@ rpkm <- tmp%>%
   pivot_wider(id_cols = gene,
               values_from = "RPKM",
               names_from = "sample_name")
-rpkm[is.na(rpkm)] <- 0
+rpkm[is.na(rpkm)] <- "0"
 write.table(rpkm,"rpkm.abd.txt",
                     sep = "\t",
                     quote = F,row.names = F)
@@ -223,7 +223,7 @@ com <- tmp%>%
   pivot_wider(id_cols = gene,
               values_from = "GAM",
               names_from = "sample_name")
-com[is.na(com)] <- 0
+com[is.na(com)] <- "0"
 com <- com[!duplicated(com[,c(1,2)]),]
 write.table(com,"GAM.abd.txt",
                     sep = "\t",
