903f1f263798b6bd72547f38d249bbd73c58939e
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2025-02-19 19:42:33 +0800

    Update comts_rpkm.help.r

--- bins/comts_rpkm.help.r
+++ bins/comts_rpkm.help.r
@@ -7,7 +7,7 @@ option_list <- list(
   make_option(c("--help","-h"),type = "character",default = F,
               help = "Show this help message and exit"))
 opt_parser = OptionParser(
-  usage = "comts geneset ribo     To calculate RPKM abandance of 14 universal single copy ribosomal genes (USCGs).\n       comts geneset res      To convert RPKM to GAM of single copy gene through GeneSet.",
+  usage = "comts geneset ruscg     To calculate RPKM abandance of 14 universal single copy ribosomal genes (USCGs).\n       comts geneset res      To convert RPKM to GAM of single copy gene through GeneSet.",
   add_help_option = TRUE,
   prog=NULL ,
   description = "This page is to show how to run the comts.")
