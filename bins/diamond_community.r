a5a2866794140ca0968d11854a9f216e4b1afd58
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2025-02-19 09:56:09 +0800

    Update diamond_community.r

--- bins/diamond_community.r
+++ bins/diamond_community.r
@@ -35,9 +35,9 @@ wd1 <- dirname(opt$SCG_db)
 wd2 <- dirname(opt$USCG_db)
 diamond_db <- paste0(wd1,"/",basename(opt$SCG_db))
 singleM <- paste0(wd2,"/",basename(opt$USCG_db))
+input_reads <- file.path(getwd(),dirname(input_reads))
 setwd(dirname(opt$result))
 outpath <- basename(opt$result)
-input_reads <- opt$input_reads
 threads <- opt$threads
 fastp_output <- paste0(outpath,".filtered.fq.gz")
 min_length <- opt$min_length
